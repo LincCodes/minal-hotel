9356ab7e03b0ef771dd5ee6bc7e788c40d7b896f
DukeLishebo <103173513+DukeLishebo@users.noreply.github.com>
2022-09-12 12:11:24 +0100

    Saved change

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -26,6 +26,7 @@
                     <li class="nav-item"><a class="nav-link" href="#">ABOUT</a></li>
                     <li class="nav-item"><a class="nav-link" href="#">CONTACT</a></li>
                 </ul><a class="btn btn-primary" href="login">LOG IN</a>
+                </ul><a class="btn btn-primary" href="signup">SIGN UP</a>
             </div>
         </div>
     </nav>
