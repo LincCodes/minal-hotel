474267290f947d1c9266652acd7f9201254ba9df
DukeLishebo <103173513+DukeLishebo@users.noreply.github.com>
2022-09-12 11:36:01 +0100

    Change

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -20,7 +20,7 @@
         <div class="container"><a class="navbar-brand" href="/">Minal Hotel</a><button data-bs-toggle="collapse" data-bs-target="#navbarResponsive" class="navbar-toggler navbar-toggler-right" type="button" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ms-auto text-uppercase">
-                    <li class="nav-item"><a class="nav-link" href="/">HOME</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/">HOME_PAGE</a></li>
                     <li class="nav-item"></li>
                     <li class="nav-item"></li>
                     <li class="nav-item"><a class="nav-link" href="#">ABOUT</a></li>
